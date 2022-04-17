d45dafdb358fb09462441c19ad0dc11c6a9c2299
vundeja <102342132+vundeja@users.noreply.github.com>
2022-04-17 12:45:07 +0530

    Added file_op file

---
+++ file_op.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+
+filename = r"C:\Temp\python_project\hello.txt"
+currenttime = datetime.now().strftime("%m-%d-%Y, %H:%M:%S")
+content = "Hello World!" + " Timestamp is: " + currenttime
+with open(filename, "a+") as f:
+    f.writelines("\n" + content)
+f.close()
