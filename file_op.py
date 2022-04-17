978ca126dcd5072fbf70eeb27992f10a4c0fc9cb
vundeja <102342132+vundeja@users.noreply.github.com>
2022-04-17 13:39:41 +0530

    Update file_op.py

--- file_op.py
+++ file_op.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 filename = r"C:\Temp\python_project\hello.txt"
 currenttime = datetime.now().strftime("%m-%d-%Y, %H:%M:%S")
-content = "Hello World!" + " Github Timestamp is: " + currenttime
+content = "Hello World!" + " Github1 Timestamp is: " + currenttime
 with open(filename, "a+") as f:
     f.writelines("\n" + content)
 f.close()
